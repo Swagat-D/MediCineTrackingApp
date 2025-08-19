0cbb82a88d7e10bf146c442e58bb5aa99cf96e37
Swagat-D <swagatdash164@gmail.com>
2025-08-19 12:58:42 +0530

    updated

--- app.config.js
+++ app.config.js
@@ -5,17 +5,51 @@ export default {
     name: "MediTracker",
     slug: "meditracker-app",
     version: "1.0.0",
-    sdkVersion: "53.0.0",
     orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "medicinetrackingapp",
     userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true
+    },
     android: {
-      "jsEngine": "jsc",
       package: "com.swagat1212.meditrackerapp",
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
       edgeToEdgeEnabled: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-splash-screen",
+      "expo-font",
+      "expo-camera",
+      "expo-location",
+      "expo-notifications",
+      [
+        "expo-build-properties",
+        {
+          android: {
+            compileSdkVersion: 35,
+            targetSdkVersion: 35,
+            buildToolsVersion: "35.0.0",
+            minSdkVersion: 24
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
     },
     extra: {
       eas: {
--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add resolver configuration to help with Android build issues
+config.resolver.platforms = ['native', 'android', 'ios', 'web'];
+config.resolver.assetExts.push('db', 'mp3', 'ttf', 'obj', 'png', 'jpg');
+
 module.exports = config;
